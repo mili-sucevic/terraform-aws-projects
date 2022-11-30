c208d0ff6a29b8e68698a4b7f0b1608d79cd92ef
Milica Sucevic <milicasucevic@gmail.com>
2022-11-30 13:50:59 +0000

    tf demo

--- learning-tf-notes-guide/first_ec2.tf
+++ learning-tf-notes-guide/first_ec2.tf
@@ -1,20 +1,16 @@
 # Provider Block
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA3Z2RFG2X3O57ZAXX"
-  secret_key = "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"
+  region = "us-east-1"
 }
 
-/*
 # Resource Block
 resource "aws_instance" "first-ec2" {
   ami           = "ami-0b0dcb5067f052a63"
-  instance_type = "t2.micro"
+  instance_type = var.instancetype
   tags = {
     Name = "first-ec2"
   }
 }
-*/
 
 resource "aws_eip" "lb" {
   vpc = true
@@ -23,15 +19,18 @@ resource "aws_eip" "lb" {
   }
 }
 
+/*
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = "mili-attributes-demo"
 }
-
+*/
 
 output "aws_eip" {
   value = aws_eip.lb.public_ip
 }
 
+/*
 output "my-s3-bucket" {
   value = aws_s3_bucket.s3-bucket.bucket_domain_name
 }
+*/
---
+++ learning-tf-notes-guide/variables.tf
@@ -0,0 +1,9 @@
+/*
+variable "vpn_ip" {
+  default = "116.50.30.20/32"
+}
+*/
+
+variable "instancetype" {
+  default = "t2.micro"
+}
